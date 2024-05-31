b5a79e65281200d09382806cf8d0be3efaa37782
John Cho <yohancho316@gmail.com>
2024-05-31 13:47:11 -0700

    defined main class

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Model;
 import view.CircleNode;
+import view.View;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
@@ -10,12 +11,26 @@ import javafx.scene.paint.Color;
 
 public class Controller {
 	
+	// View Class Node
+	private View view;
+	
 	// Model Class Node
 	private Model model;
+	
+	// View Getter Method
+	public View getView() {
+		return this.view;
+	}
 	
 	// Model Getter Method
 	public Model getModel() {
 		return this.model;
+	}
+	
+	// View Setter Method
+	public void setView(View view) {
+		if(view == null) throw new NullPointerException("View cannot be null");
+		this.view = view;
 	}
 	
 	// Model Setter Method
@@ -51,12 +66,12 @@ public class Controller {
 			// Print Row/Column Position of Circle
 			System.out.println("Selected Circle (" + circle.getRow() + "/" + circle.getColumn() + ")");
 			
+			// IMPLEMENT CHECK FOR GAME PLAY IS OVER
 			if(getModel().getGamePlay() == false) { System.out.println("Game is over!"); }
-			
 			
 			// Check for Player's Turn
 			if(getModel().getRedTurn() == true) {
-				
+				 
 				// Change to Yellow Player's Turn
 				model.changePlayerTurn();
 				
---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+
+import controller.Controller;
+import model.Model;
+import view.View;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	
+	@Override
+	public void start(Stage rootprimaryStage) {
+		
+		// Instantiate Model Node
+		Model model = new Model();
+		
+		
+		// Instantiate View Node
+		
+		// Instantiate Controller Node
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
